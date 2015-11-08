381ccfadb54d81efcc08280868deaec3b2162818
Giovanni Carvelli <giovannircarvelli@gmail.com>
2015-11-08 00:13:05 -0500

    Get rid of nasty html special characters

--- question_get.php
+++ question_get.php
@@ -4,4 +4,4 @@ $q = new Question();
 $sites = array('stackoverflow', 'ux', 'unix', 'askubuntu', 'softwarerecs', 'programmers');
 $q->add_sites($sites);
 $q->send_question();
-print_r(json_encode($q));
+echo htmlspecialchars_decode(json_encode($q), ENT_QUOTES);
