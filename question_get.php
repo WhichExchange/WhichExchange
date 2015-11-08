ccfd40678d40ee1759966109f8f5386a21c0ed0d
Giovanni Carvelli <giovannircarvelli@gmail.com>
2015-11-08 01:29:52 -0500

    Added serverfault and superuser to list of acceptable stack exchanges

--- question_get.php
+++ question_get.php
@@ -1,7 +1,16 @@
 <?php
 require_once 'Question.php'; //fugg
 $q = new Question();
-$sites = array('stackoverflow', 'ux', 'unix', 'askubuntu', 'softwarerecs', 'programmers');
+$sites = array(
+    'stackoverflow',
+    'ux',
+    'unix',
+    'askubuntu',
+    'softwarerecs',
+    'programmers',
+    'serverfault',
+    'superuser'
+);
 $q->add_sites($sites);
 $q->send_question();
 echo htmlspecialchars_decode(json_encode($q), ENT_QUOTES);
--- sites.php
+++ sites.php
@@ -1,6 +1,16 @@
 <?php
 
 // TODO add more sites -- hard coded for now
-$sites = array('stackoverflow', 'ux', 'unix', 'askubuntu', 'softwarerecs', 'programmers');
+$sites = array(
+    'stackoverflow',
+    'ux',
+    'unix',
+    'askubuntu',
+    'softwarerecs',
+    'programmers',
+    'serverfault',
+    'superuser'
+);
+
 echo json_encode($sites);
 
