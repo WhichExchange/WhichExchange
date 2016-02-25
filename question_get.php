6b5f84802c15aed35957e47b22047a69e56f5a2f
Giovanni Carvelli <giovannircarvelli@gmail.com>
2016-02-24 23:05:22 -0500

    put sites in json file instead of hardcoded php

--- question_get.php
+++ question_get.php
@@ -1,16 +1,7 @@
 <?php
 require_once 'Question.php'; //fugg
 $q = new Question();
-$sites = array(
-    'stackoverflow',
-    'ux',
-    'unix',
-    'askubuntu',
-    'softwarerecs',
-    'programmers',
-    'serverfault',
-    'superuser'
-);
-$q->addSites($sites);
+$sites = json_decode(file_get_contents('sites.json'));
+$q->addSites($sites->sites);
 $q->sendQuestion();
 echo htmlspecialchars_decode(json_encode($q), ENT_QUOTES);
